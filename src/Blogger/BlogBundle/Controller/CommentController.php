c4a0f34fceae11397d6f0ef3d3d61bd84398e646
hubgitse <hub.2010@mail.ru>
2017-09-22 09:10:04 +0400

    upload

---
+++ src/Blogger/BlogBundle/Controller/CommentController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Blogger\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Blogger\BlogBundle\Entity\Comment;
+use Blogger\BlogBundle\Form\CommentType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Mapping\ClassMetadata;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+/**
+ * Comment controller.
+ */
+class CommentController extends Controller
+{
+    public function newAction($blog_id)
+    {
+        $blog = $this->getBlog($blog_id);
+
+        $comment = new Comment();
+        $comment->setBlog($blog);
+        $form = $this->createForm(CommentType::class, $comment);
+
+        return $this->render('BloggerBlogBundle:Comment:form.html.twig', array(
+            'comment' => $comment,
+            'form' => $form->createView()
+        ));
+    }
+
+    public function createAction(Request $request, $blog_id)
+    {
+        $blog = $this->getBlog($blog_id);
+
+        $comment = new Comment();
+        $comment->setBlog($blog);
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()
+                ->getManager();
+            $em->persist($comment);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('BloggerBlogBundle_blog_show', array(
+                    'id'    => $comment->getBlog()->getId(),
+                    'slug'  => $comment->getBlog()->getSlug())) .
+                '#comment-' . $comment->getId()
+            );
+        }
+
+        return $this->render('BloggerBlogBundle:Comment:create.html.twig', array(
+            'comment' => $comment,
+            'form' => $form->createView()
+        ));
+    }
+
+
+    public static function loadValidatorMetadata(ClassMetadata $metadata)
+    {
+        $metadata->addPropertyConstraint('user', new NotBlank(array(
+            'message' => 'You must enter your name'
+        )));
+        $metadata->addPropertyConstraint('comment', new NotBlank(array(
+            'message' => 'You must enter a comment'
+        )));
+    }
+
+
+    protected function getBlog($blog_id)
+    {
+        $em = $this->getDoctrine()
+            ->getManager();
+
+        $blog = $em->getRepository('BloggerBlogBundle:Blog')->find($blog_id);
+
+        if (!$blog) {
+            throw $this->createNotFoundException('Unable to find Blog post.');
+        }
+
+        return $blog;
+    }
+}
